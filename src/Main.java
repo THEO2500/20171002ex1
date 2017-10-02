94c6d4a17500d306c8a347ee08b69cd86facd1b9
THEO2500 <worldpeace871219@gmail.com>
2017-10-02 14:30:39 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+    String str =in.nextLine();
+	System.out.println(str.length());
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+    }
+}
